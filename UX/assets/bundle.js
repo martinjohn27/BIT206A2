da0b63d4ee7a5946171a212a8fc55ce7ffabcb19
EdisonTan11 <59584716+EdisonTan11@users.noreply.github.com>
2020-03-26 09:52:32 +0800

    navigation bar

--- UX/assets/bundle.js
+++ UX/assets/bundle.js
@@ -1,20 +1,29 @@
+const elements = {
+  homeNavigation :document.getElementById("navigation1"),
+  navigation : document.getElementById("navigation")
+}
+
+
 
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navigation").style.marginTop = "-150px";
+    elements.navigation === null ? elements.homeNavigation.style.marginTop = "-180px"
+    :   elements.navigation.style.marginTop = "50px"; 
     $(".navigation img").addClass("big-image");
     $(".navigation span").addClass("visibility");
     $(".navigation span").hide();
-    document.getElementById("navigation").style.opacity = "0.9";
-    
+    elements.navigation === null ? elements.homeNavigation.style.opacity = "0.9" 
+    : elements.navigation.style.opacity = "0.9";
   } else {
-    document.getElementById("navigation").style.marginTop = "-200px";
+    elements.navigation === null ? elements.homeNavigation.style.marginTop = "-220px"
+    : elements.navigation.style.marginTop = "-50px";
     $(".navigation img").removeClass("big-image");
     $(".navigation span").removeClass("visibility");  
     $(".navigation span").show();
-    document.getElementById("navigation").style.opacity = "1";
+    elements.navigation === null ? elements.homeNavigation.style.opacity = "1"
+    : elements.navigation.style.opacity = "1";
   }
 }
 
@@ -29,7 +38,8 @@ $(".navigation").mouseover(function(){
     $(".navigation span").show();
     $(".navigation span").removeClass("visibility");
     $("img").removeClass("big-image");
-    document.getElementById("navigation").style.marginTop = "-4px";
+    elements.navigation === null ? elements.homeNavigation.style.marginTop = "-300px"
+    : elements.navigation.style.marginTop = "-10px";
   }
 });
 
@@ -40,6 +50,7 @@ $(".navigation").mouseout(function(){
     $(".navigation span").hide();
     $(".navigation span").addClass("visibility");
     $(".navigation img").addClass("big-image");
-    document.getElementById("navigation").style.marginTop = "-150px";
+    elements.navigation === null ? elements.homeNavigation.style.marginTop = "-180px"
+    : elements.navigation.style.marginTop = "50px";
   }
 });
